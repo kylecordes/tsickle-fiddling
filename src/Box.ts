d75c69e05d01ef288dc8ef78f46ed0b4707066ec
Kyle Cordes <kyle.cordes@oasisdigital.com>
2017-03-06 22:11:58 -0600

    Simplify case down to almost nothing

--- src/Box.ts
+++ src/Box.ts
@@ -1,16 +1,4 @@
-import { Apple } from './Apple';
 
 export class Box {
-  private apple: Apple;
 
-  constructor() {
-    this.apple = new Apple();
-  }
-
-  go() {
-    const result = this.apple.eatApple((x) => 'Hello, here is the output: ' + x);
-    console.log(result);
-  }
 }
-
-new Box().go();
