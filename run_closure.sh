e1fa05f91873063ed96d54e56e8113053e40677b
Kyle Cordes <kyle.cordes@oasisdigital.com>
2017-03-06 21:47:00 -0600

    Working, very small example with tsickle and Closure Compiler.
    remove -/_ from output dir - there seem to be rules for that
    
    also nail dependencies more specifically.
    
    wip

--- run_closure.sh
+++ run_closure.sh
@@ -4,11 +4,15 @@ set -ex
 OPTS=(
   "--language_in=ES6_STRICT"
   "--language_out=ES5"
+  "--compilation_level=ADVANCED_OPTIMIZATIONS"
   "--js_output_file=output.js"
 
-  # "--js_module_root=tsickle-output"
+  "--js_module_root=built"
 
-  "tsickle-output/*.js"
+  "built/*.js"
+
+  "--entry_point=built.Box"
+  "--dependency_mode=STRICT"
 )
 
 closureFlags=$(mktemp)
